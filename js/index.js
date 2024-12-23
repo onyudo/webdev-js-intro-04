8cf2f3d2dea4207ffc3ae31e16b68d320e7b76f1
“Martin <martin@forceandform.com>
2024-12-23 13:38:03 -0500

    Completed the experiment section inside of index.js and commented out all code once complete. Updated README to indicate that Tasks Part 1 have been completed.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,73 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict";
+//"use strict"
+
+/*
+
+// Copy the last code block into your script and change the hero's health points to see what logic gets executed.
+
+let healthPoints = 3;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+}
+
+// Create a couple if statements that are meaningful.
+
+let numberOfCookies = 15
+
+if (numberOfCookies > 10) {
+    console.log("Thatsa lotta cookies");
+}
+
+let martinis = 20
+
+if (martinis > 3) {
+    console.log("You're having too much to drink");
+}
+
+// Create a couple if/else statements that are meaningful.
+
+let numberOfCookies = 1
+
+if (numberOfCookies > 10) {
+    console.log("Thatsa lotta cookies");
+} else {
+    console.log("You can have another cookie if you want");
+}
+
+let martinis = 2
+
+if (martinis < 3) {
+    console.log("You don't need another drink");
+} else {
+    console.log("You had too much to drink tonight");
+}
+
+// Create a couple if/else if/else statements that are meaningful.
+
+let numberOfCookies = 100
+
+if (numberOfCookies <= 2) {
+    console.log("You've had plenty of cookies");
+} else if (numberOfCookies < 5 && numberOfCookies >= 10 ) {
+    console.log("You ate way too many cookies");
+} else {
+    console.log("You're going to eat yourself to death");
+}
+
+let martinis = 2
+
+if (martinis < 3) {
+    console.log("You've had plenty to drink tonight");
+} else if (martinis < 5 && numberOfCookies >= 10 ) {
+    console.log("You drank way too many martinis");
+} else {
+    console.log("How are you even still waling at this point?");
+}
+
+*/
